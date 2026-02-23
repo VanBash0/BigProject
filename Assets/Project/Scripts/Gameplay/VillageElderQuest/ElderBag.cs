3a7dcde46a7319ad43ec6f2fb051956ceda24bbf
Семён <71784144+bonbonse@users.noreply.github.com>
2026-02-23 16:44:33 +0300

    Feature/elder bag (#162)
    * промежуточный коммит
    
    * сумка старейшины

---
+++ Assets/Project/Scripts/Gameplay/VillageElderQuest/ElderBag.cs
@@ -0,0 +1,27 @@
+using BigProject.Intercatable;
+using BigProject.Managers;
+using BigProject.Systems;
+using UnityEngine;
+
+namespace BigProject.Gameplay.VillageElderQuest
+{
+    public class ElderBag : MonoBehaviour, IInteractable
+    {
+        [SerializeField]
+        private int _inventoryItemId;
+
+        private InventorySystem _inventory;
+
+        public void Init(InventorySystem inventory)
+        {
+            _inventory = inventory;
+        }
+        
+        public void Interact()
+        {
+            _inventory.AddItemByItemID(_inventoryItemId);
+            Destroy(gameObject);
+        }
+    }
+}
+
