a824c8a5e5d05a736e5f12b608f9509e68bfbf13
Malorepa <vladimirfrol@mail.ru>
2026-02-24 16:34:52 +0300

    Настроил перезагрузку сцены для тестов

--- Assets/Project/Scripts/Gameplay/TownHall/TestSceneInput.cs
+++ Assets/Project/Scripts/Gameplay/TownHall/TestSceneInput.cs
@@ -1,3 +1,6 @@
+using BigProject.Managers;
+using BigProject.Systems;
+using BigProject.Systems.QuestSystem;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -13,7 +16,15 @@ namespace BigProject.Utilities
                 if (Keyboard.current.escapeKey.wasPressedThisFrame)
                     Application.Quit();
                 else if (Keyboard.current.rKey.wasPressedThisFrame)
+                {
+                    ServiceLocator.ReleaseService<ProgressManager>();
+                    ServiceLocator.AddService(new ProgressManager("Player", new QuestJsonLoader("Data\\Quests"), new()));
+                    var inv = ServiceLocator.GetService<InventorySystem>();
+                    for (int i = 0; i < 15; i++)
+                        if (inv.HasItemByID(i))
+                            inv.RemoveItemById(i);
                     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
             }
         }
     }
